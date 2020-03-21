6a22ca72304461dbbbb599796645ce35cec44a2e
JimLee1996 <59061463@qq.com>
2020-03-21 15:54:32 +0800

    use DepthwiseSeparableFPN

---
+++ backbone/fpn.py
@@ -0,0 +1,133 @@
+import math
+import torch.nn.functional as F
+
+import fvcore.nn.weight_init as weight_init
+
+from detectron2.layers import Conv2d, ShapeSpec, get_norm
+from detectron2.modeling.backbone.backbone import Backbone
+from detectron2.modeling.backbone.fpn import FPN, _assert_strides_are_log2_contiguous
+
+
+class DepthwiseSeparableFPN(Backbone):
+
+    def __init__(self,
+                 bottom_up,
+                 in_features,
+                 out_channels,
+                 norm="",
+                 top_block=None,
+                 fuse_type="sum"):
+        super(DepthwiseSeparableFPN, self).__init__()
+        assert isinstance(bottom_up, Backbone)
+
+        input_shapes = bottom_up.output_shape()
+        in_strides = [input_shapes[f].stride for f in in_features]
+        in_channels = [input_shapes[f].channels for f in in_features]
+
+        _assert_strides_are_log2_contiguous(in_strides)
+        lateral_convs = []
+        output_convs = []
+
+        use_bias = norm == ""
+        for idx, in_channels in enumerate(in_channels):
+            lateral_norm = get_norm(norm, out_channels)
+            output_norm = get_norm(norm, out_channels)
+
+            # pointwise
+            lateral_conv = Conv2d(in_channels,
+                                  out_channels,
+                                  kernel_size=1,
+                                  bias=use_bias,
+                                  norm=lateral_norm)
+            # depthwise
+            output_conv = Conv2d(out_channels,
+                                 out_channels,
+                                 kernel_size=3,
+                                 stride=1,
+                                 padding=1,
+                                 bias=use_bias,
+                                 norm=output_norm,
+                                 groups=out_channels)
+            weight_init.c2_xavier_fill(lateral_conv)
+            weight_init.c2_xavier_fill(output_conv)
+            stage = int(math.log2(in_strides[idx]))
+            self.add_module("fpn_lateral{}".format(stage), lateral_conv)
+            self.add_module("fpn_output{}".format(stage), output_conv)
+
+            lateral_convs.append(lateral_conv)
+            output_convs.append(output_conv)
+        # Place convs into top-down order (from low to high resolution)
+        # to make the top-down computation in forward clearer.
+        self.lateral_convs = lateral_convs[::-1]
+        self.output_convs = output_convs[::-1]
+        self.top_block = top_block
+        self.in_features = in_features
+        self.bottom_up = bottom_up
+        # Return feature names are "p<stage>", like ["p2", "p3", ..., "p6"]
+        self._out_feature_strides = {
+            "p{}".format(int(math.log2(s))): s for s in in_strides
+        }
+        # top block output feature maps.
+        if self.top_block is not None:
+            for s in range(stage, stage + self.top_block.num_levels):
+                self._out_feature_strides["p{}".format(s + 1)] = 2**(s + 1)
+
+        self._out_features = list(self._out_feature_strides.keys())
+        self._out_feature_channels = {
+            k: out_channels for k in self._out_features
+        }
+        self._size_divisibility = in_strides[-1]
+        assert fuse_type in {"avg", "sum"}
+        self._fuse_type = fuse_type
+
+    @property
+    def size_divisibility(self):
+        return self._size_divisibility
+
+    def forward(self, x):
+        """
+        Args:
+            input (dict[str->Tensor]): mapping feature map name (e.g., "res5") to
+                feature map tensor for each feature level in high to low resolution order.
+
+        Returns:
+            dict[str->Tensor]:
+                mapping from feature map name to FPN feature map tensor
+                in high to low resolution order. Returned feature names follow the FPN
+                paper convention: "p<stage>", where stage has stride = 2 ** stage e.g.,
+                ["p2", "p3", ..., "p6"].
+        """
+        # Reverse feature maps into top-down order (from low to high resolution)
+        bottom_up_features = self.bottom_up(x)
+        x = [bottom_up_features[f] for f in self.in_features[::-1]]
+        results = []
+        prev_features = self.lateral_convs[0](x[0])
+        results.append(self.output_convs[0](prev_features))
+        for features, lateral_conv, output_conv in zip(x[1:],
+                                                       self.lateral_convs[1:],
+                                                       self.output_convs[1:]):
+            top_down_features = F.interpolate(prev_features,
+                                              scale_factor=2,
+                                              mode="nearest")
+            lateral_features = lateral_conv(features)
+            prev_features = lateral_features + top_down_features
+            if self._fuse_type == "avg":
+                prev_features /= 2
+            results.insert(0, output_conv(prev_features))
+
+        if self.top_block is not None:
+            top_block_in_feature = bottom_up_features.get(
+                self.top_block.in_feature, None)
+            if top_block_in_feature is None:
+                top_block_in_feature = results[self._out_features.index(
+                    self.top_block.in_feature)]
+            results.extend(self.top_block(top_block_in_feature))
+        assert len(self._out_features) == len(results)
+        return dict(zip(self._out_features, results))
+
+    def output_shape(self):
+        return {
+            name: ShapeSpec(channels=self._out_feature_channels[name],
+                            stride=self._out_feature_strides[name])
+            for name in self._out_features
+        }
--- backbone/shufflenet.py
+++ backbone/shufflenet.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
 
-from detectron2.modeling import BACKBONE_REGISTRY, Backbone, FPN, ShapeSpec
+from detectron2.modeling import BACKBONE_REGISTRY, Backbone, ShapeSpec
 from detectron2.modeling.backbone.fpn import LastLevelMaxPool
+
+from .fpn import DepthwiseSeparableFPN
+
 
 
 def channel_shuffle(x, groups):
@@ -263,7 +266,7 @@ def build_shufflenetv2_fpn_backbone(cfg):
     bottom_up = build_shufflenetv2_backbone(cfg)
     in_features = cfg.MODEL.FPN.IN_FEATURES
     out_channels = cfg.MODEL.FPN.OUT_CHANNELS
-    backbone = FPN(
+    backbone = DepthwiseSeparableFPN(
         bottom_up=bottom_up,
         in_features=in_features,
         out_channels=out_channels,
@@ -276,7 +279,7 @@ def build_shufflenetv2_fpn_backbone(cfg):
 
 
 if __name__ == "__main__":
-    from config import cfg
+    from .config import cfg
     model = build_shufflenetv2_fpn_backbone(cfg)
     print(model)
 
